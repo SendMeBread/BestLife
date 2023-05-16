79a25ef0680819f0c0b67bd2687ca03743cd570b
SendMeMyBread <joemczero@gmail.com>
2023-05-15 22:10:51 -0400

    Update Graphs.py

--- Data Collection/Graphs.py
+++ Data Collection/Graphs.py
@@ -1,13 +1,9 @@
 import numpy as np
-import matplotlib as mpl
+from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
-t_s = 2.5
-t_a = (((972 - t_s)/712) + ((122 - t_s)/103))/2
-x = np.arange(-80, 1, 80)
-print("Values of x:", x)
-y = x * t_a
-plt.plot(x, y)
-plt.title("BIBO 3D Print Time Deviation")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.show()
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+zline = np.linspace(0, 1, 2)
+xline = np.sin(zline)
+yline = np.cos(zline)
+ax.plot(xline, yline, zline, 'gray')
