b6bee5e7951b5982f2570428aadb8fea0358d398
SendMeMyBread <joemczero@gmail.com>
2023-06-02 15:11:56 -0400

    Did stuff

--- Data Collection/Graphs.py
+++ Data Collection/Graphs.py
@@ -1,9 +1,19 @@
 import numpy as np
-from mpl_toolkits import mplot3d
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-zline = np.linspace(0, 1, 2)
-xline = np.sin(zline)
-yline = np.cos(zline)
-ax.plot(xline, yline, zline, 'gray')
+from mpl_toolkits import mplot3d
+x1 = np.linspace(0, 85, 85)
+y1 = -(1945/37584)*x1**2 + (1/.58)*x1 + 5
+x2 = np.linspace(0, 85, 85)
+y2 = -(2595/69629)*x2**2 + (1/.58)*x2 + 5
+x3 = np.linspace(0, 85, 85)
+y3 = -(3345/118784)*x3**2 + (1/.58)*x3 + 5
+x4 = np.linspace(0, 85, 85)
+y4 = -(1495/21141)*x4**2 + (1/.58)*x4 + 5
+x5 = np.linspace(0, 85, 85)
+y5 = -(479/11745)*x5**2 + (1/.58)*x5 + 5
+
+plt.plot(x1, y1, "r", x2, y2, "g", x3, y3, "b", x4, y4, "y", x5, y5, "k")
+plt.xlim(left=0)
+plt.ylim(bottom=0)
+plt.show()
