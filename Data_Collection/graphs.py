8152d1c1c805d503ff456b34fd7062fe6daa05d0
SendMeMyBread <joemczero@gmail.com>
2023-06-08 18:28:32 -0400

    Update graphs.py

--- Data_Collection/graphs.py
+++ Data_Collection/graphs.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 data = {'Live':0, 'Die':10}
 live_die = list(data.keys())
 kdr = list(data.values())
-fig = plt.figure(figsize=(10,5))
+fig = plt.figure(figsize= (10,5))
 plt.bar(live_die, kdr, color='maroon', width=.5)
 plt.ylabel('Did they live?')
 plt.title('Live or die?')
