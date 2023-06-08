0ad455405cc984ec1e7a346bc1b9268131519378
SendMeMyBread <joemczero@gmail.com>
2023-06-08 18:14:15 -0400

    Update graphs.py

--- Data_Collection/graphs.py
+++ Data_Collection/graphs.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
-def kd_bar():
-        data = {'Live':0, 'Die':10}
-        live_die = list(data.keys())
-        kdr = list(data.values())
-
-        plt.bar(live_die, kdr, color='maroon', width=.5)
-        plt.ylabel('Did they live?')
-        plt.title('Live or die?')
-        plt.show()
-kd_bar()
+data = {'Live':0, 'Die':10}
+live_die = list(data.keys())
+kdr = list(data.values())
+fig = plt.figure(figsize=(10,5))
+plt.bar(live_die, kdr, color='maroon', width=.5)
+plt.ylabel('Did they live?')
+plt.title('Live or die?')
+plt.show()
