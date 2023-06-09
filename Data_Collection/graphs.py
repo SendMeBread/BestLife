e83f97aea964ff39c81413d4f3e91c10d85b7df4
SendMeMyBread <joemczero@gmail.com>
2023-06-08 21:49:09 -0400

    Update graphs.py

--- Data_Collection/graphs.py
+++ Data_Collection/graphs.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 vals = np.array([0, 100])
 labels = ['Lived', 'Died']
 colors = ['#00d', '#c00']
-plt.pie(vals, colors=colors)
-plt.legend(labels, title="Did they live?")
-plt.show()
+
+def plot():
+    fig, ax = plt.subplots()
+    ax.pie(vals, colors=colors)
+    ax.legend(labels title="Did They Live?")
+    display(fig, target="graph-area", append=False)
+
+plot()
