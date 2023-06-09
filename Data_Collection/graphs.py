1663937b6a0a5e70697252b32d8e04509f6e37a7
SendMeMyBread <joemczero@gmail.com>
2023-06-09 19:40:29 -0400

    wow

--- Data_Collection/graphs.py
+++ Data_Collection/graphs.py
@@ -6,6 +6,15 @@ import mpld3
 vals = np.array([0, 100])
 labels = ['Lived', 'Died']
 colors = ['#00d', '#c00']
+plt.figure(facecolor="#000")
+plt.axes().set_facecolor("#000")
 plt.pie(vals, colors=colors)
-plt.legend(labels, title="Did they live?")
-mpld3.save_html(plt.figure(), 'why_not.html', template_type='simple')
+legend = plt.legend(labels, title="Did They Live?")
+frame = legend.get_frame()
+frame.set_facecolor("#000")
+frame.set_edgecolor("#f00")
+frame.set_alpha(.75)
+plt.setp(legend.get_title(), color="#f00")
+for text in legend.get_texts():
+    text.set_color("#0f0")
+mpld3.save_html(plt.figure(), "why.html", template_type="simple")
