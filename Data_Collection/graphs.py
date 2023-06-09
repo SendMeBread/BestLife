fb2a07a0b688c13738aed8fc9ecf5f9bb1596ac7
SendMeMyBread <joemczero@gmail.com>
2023-06-08 22:11:17 -0400

    Did way too much crap here.

--- Data_Collection/graphs.py
+++ Data_Collection/graphs.py
@@ -1,14 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import mpld3
+
 
 vals = np.array([0, 100])
 labels = ['Lived', 'Died']
 colors = ['#00d', '#c00']
-
-def plot():
-    fig, ax = plt.subplots()
-    ax.pie(vals, colors=colors)
-    ax.legend(labels, title="Did They Live?")
-    display(fig, target="graph-area", append=False)
-
-plot()
+plt.pie(vals, colors=colors)
+plt.legend(labels, title="Did they live?")
+mpld3.show()
+mpld3.save_html(plt.figure(), 'why_not.html')
