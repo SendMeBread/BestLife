8bc67cc142b1d8cf4ac34de43242f289071c90bd
SendMeMyBread <joemczero@gmail.com>
2023-06-08 21:23:27 -0400

    Update graphs.py

--- Data_Collection/graphs.py
+++ Data_Collection/graphs.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
-data = {'Live':0, 'Die':10}
-live_die = list(data.keys())
-kdr = list(data.values())
-fig = plt.figure(figsize= (10,5))
-plt.bar(live_die, kdr, color='maroon', width=.5)
-plt.ylabel('Did they live?')
-plt.title('Live or die?')
-fig
+import numpy as np
+
+vals = np.array([0, 100])
+labels = ['Lived', 'Died']
+colors = ['#00d', '#c00']
+plt.pie(vals, colors=colors)
+plt.legend(labels, title="Did they live?")
+plt.show()
